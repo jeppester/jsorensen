0edab05a316524a2a2fed935889028f2c8033a13
Jesper Sørensen <jeppester1@gmail.com>
2025-01-27 22:50:59 +0100

    Upgrade from node-sass to sass

--- util/compileSass.mjs
+++ util/compileSass.mjs
@@ -1,15 +1,11 @@
 import fs from 'fs'
-import { promisify } from 'util'
 import path from 'path'
-import sass from 'node-sass'
+import sass from 'sass'
 
 const fsPromises = fs.promises
-const renderSass = promisify(sass.render).bind(sass)
 
 export default async (stylesPath, publicPath) => {
-  const result = await renderSass({
-    file: path.join(stylesPath, 'style.scss'),
-  })
+  const result = await sass.compileAsync(path.join(stylesPath, 'style.scss'))
 
   const fileHandle = await fsPromises.open(path.join(publicPath, `style.css`), 'w')
   await fileHandle.write(result.css)
